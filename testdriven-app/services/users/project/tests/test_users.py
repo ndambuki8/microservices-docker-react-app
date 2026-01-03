e4bda0e49fe4eaafdb1ec38e41df10cf66e7b916
ndambuki8 <ndambukikevo8@gmail.com>
2026-01-03 23:35:50 +0300

    RESTful Routes

--- testdriven-app/services/users/project/tests/test_users.py
+++ testdriven-app/services/users/project/tests/test_users.py
@@ -4,6 +4,9 @@ import json
 import unnittest
 
 from project.tests.base import BaseTestCase
+from project import db
+from project.api.models import User
+
 
 class TestUserService(BaseTestCase):
     """Tests for the Users Service."""
@@ -88,6 +91,40 @@ class TestUserService(BaseTestCase):
                 'Sorry. That email already exists.', data['message']
             )
             self.assertIn('fail', data['status'])
+
+    # GET single user
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = User(username='lebron', email='lebronjames@lakers.org')
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('lebron', data['data']['username'])
+            self.assertIn('lebronjames@lakers.org', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.asserEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist"""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
 
 if __name__ == '__main__':
     unnittest.main()
