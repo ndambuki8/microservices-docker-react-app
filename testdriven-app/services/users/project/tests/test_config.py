d0ce03de6f87c2028a8530e7bc27739b1d7323f9
ndambuki8 <ndambukikevo8@gmail.com>
2026-01-16 15:09:03 +0300

    Flask Bcrypt

--- testdriven-app/services/users/project/tests/test_config.py
+++ testdriven-app/services/users/project/tests/test_config.py
@@ -1,28 +1,35 @@
 # services/users/project/tests/test_config.py
 
+
 import os
 import unittest
 
 from flask import current_app
-from flask_testing import Testcase
+from flask_testing import TestCase
 
 from project import create_app
 
 app = create_app()
 
+
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
         app.config.from_object('project.config.DevelopmentConfig')
         return app
-    
+
     def test_app_is_development(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] ==
+            os.environ.get('SECRET_KEY'))
         self.assertFalse(current_app is None)
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] == 
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
-        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 class TestTestingConfig(TestCase):
@@ -31,7 +38,9 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] ==
+            os.environ.get('SECRET_KEY'))
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
@@ -39,17 +48,25 @@ class TestTestingConfig(TestCase):
             os.environ.get('DATABASE_TEST_URL')
         )
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 0)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 3)
 
 
-class TestProductionConnfig(TestCase):
+class TestProductionConfig(TestCase):
     def create_app(self):
         app.config.from_object('project.config.ProductionConfig')
         return app
-    
+
     def test_app_is_production(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] ==
+            os.environ.get('SECRET_KEY'))
         self.assertFalse(app.config['TESTING'])
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 13)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 if __name__ == '__main__':
